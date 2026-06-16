b91c0f4ad38496f1214ab18e7179352a36bceab4
Aryanmahakur <mahakuraryan@gmail.com>
2026-06-16 15:29:38 +0530

    uf

--- Singly-LinkedList.cpp
+++ Singly-LinkedList.cpp
@@ -1,69 +1,106 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class node{
-    public:
+
+// Node class representing a single element of the linked list
+class node {
+public:
     int data;
     node* next;
-    node(int value){
-        data=value;
-        next=nullptr;
+
+    node(int value) {
+        data = value;
+        next = nullptr;
     }
 };
-class singlylinkedlist{
-    public:
+
+// Singly Linked List class
+class singlylinkedlist {
+public:
     node* head;
-    singlylinkedlist(){
-        head=nullptr;
-    }
-    void insertatend(int value){
-        node* newnode=new node(value);
-        if(head==nullptr){
-            head=newnode;
-            return;
-        }
-        node* tempnode=head;
-        while(tempnode->next!=nullptr){
-            tempnode=tempnode->next;
-        }
-        tempnode->next=newnode;
-        return;
-    }
-    void deleteanode(int value){
-        node* newnode=new node(value);
 
-        if(head==newnode){
-            head=head->next;
-            return;
-        }
-        node* tempnode=head;
-        while(tempnode->next!=nullptr && tempnode->next->data!=value){
-            tempnode=tempnode->next;
-        }
-        if(tempnode->next==nullptr){
-            return;
-        }
-        node* del=tempnode->next;
+    // Constructor
+    singlylinkedlist() {
+        head = nullptr;
+    }
 
-        tempnode->next=tempnode->next->next;
+    // Insert a node at the end
+    void insertatend(int value) {
+        node* newnode = new node(value);
+
+        if (head == nullptr) {
+            head = newnode;
+            return;
+        }
+
+        node* tempnode = head;
+
+        while (tempnode->next != nullptr) {
+            tempnode = tempnode->next;
+        }
+
+        tempnode->next = newnode;
+    }
+
+    // Delete a node with the given value
+    void deleteanode(int value) {
+
+        // Empty list
+        if (head == nullptr) {
+            return;
+        }
+
+        // Delete head node
+        if (head->data == value) {
+            node* del = head;
+            head = head->next;
+            delete del;
+            return;
+        }
+
+        // Find node before target node
+        node* tempnode = head;
+
+        while (tempnode->next != nullptr &&
+               tempnode->next->data != value) {
+            tempnode = tempnode->next;
+        }
+
+        // Value not found
+        if (tempnode->next == nullptr) {
+            return;
+        }
+
+        // Delete target node
+        node* del = tempnode->next;
+        tempnode->next = tempnode->next->next;
         delete del;
-        return;
-
     }
-    void display(){
-        node* temp=head;
-        while(temp->next!=nullptr){
-            cout<<temp->data<<" -> ";
-            temp=temp->next;
+
+    // Display the linked list
+    void display() {
+        node* temp = head;
+
+        while (temp != nullptr) {
+            cout << temp->data << " -> ";
+            temp = temp->next;
         }
+
+        cout << "NULL" << endl;
     }
 };
-int main(){
+
+int main() {
     singlylinkedlist sl;
+
     sl.insertatend(10);
     sl.insertatend(20);
     sl.insertatend(30);
     sl.insertatend(40);
     sl.insertatend(50);
+
     sl.deleteanode(30);
+
     sl.display();
+
+    return 0;
 }
