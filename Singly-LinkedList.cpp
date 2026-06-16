084bf8d61affe00a4f2e7a81bdc48955780707b5
Aryanmahakur <mahakuraryan@gmail.com>
2026-06-16 15:27:03 +0530

    uf

--- Singly-LinkedList.cpp
+++ Singly-LinkedList.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 class node{
+    public:
     int data;
     node* next;
     node(int value){
@@ -8,3 +9,61 @@ class node{
         next=nullptr;
     }
 };
+class singlylinkedlist{
+    public:
+    node* head;
+    singlylinkedlist(){
+        head=nullptr;
+    }
+    void insertatend(int value){
+        node* newnode=new node(value);
+        if(head==nullptr){
+            head=newnode;
+            return;
+        }
+        node* tempnode=head;
+        while(tempnode->next!=nullptr){
+            tempnode=tempnode->next;
+        }
+        tempnode->next=newnode;
+        return;
+    }
+    void deleteanode(int value){
+        node* newnode=new node(value);
+
+        if(head==newnode){
+            head=head->next;
+            return;
+        }
+        node* tempnode=head;
+        while(tempnode->next!=nullptr && tempnode->next->data!=value){
+            tempnode=tempnode->next;
+        }
+        if(tempnode->next==nullptr){
+            return;
+        }
+        node* del=tempnode->next;
+
+        tempnode->next=tempnode->next->next;
+        delete del;
+        return;
+
+    }
+    void display(){
+        node* temp=head;
+        while(temp->next!=nullptr){
+            cout<<temp->data<<" -> ";
+            temp=temp->next;
+        }
+    }
+};
+int main(){
+    singlylinkedlist sl;
+    sl.insertatend(10);
+    sl.insertatend(20);
+    sl.insertatend(30);
+    sl.insertatend(40);
+    sl.insertatend(50);
+    sl.deleteanode(30);
+    sl.display();
+}
