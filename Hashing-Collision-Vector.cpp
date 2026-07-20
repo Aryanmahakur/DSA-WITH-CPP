61e251d81013331c17ced77ecafd4e9ad887a17a
Aryanmahakur <mahakuraryan@gmail.com>
2026-07-21 01:13:37 +0530

    uf

--- Hashing-Collision-Vector.cpp
+++ Hashing-Collision-Vector.cpp
@@ -3,62 +3,57 @@
 using namespace std;
 
 class HashTable {
+private:
+    static const int SIZE = 5;
+    vector<int> table[SIZE];
 
-
-    int hashFunction(int key)
-    {
+    int hashFunction(int key) {
         return key % SIZE;
     }
 
-    void insert(int key)
-    {
+public:
+    void insert(int key) {
         int index = hashFunction(key);
         table[index].push_back(key);
     }
 
-    bool search(int key)
-    {
+    bool search(int key) {
         int index = hashFunction(key);
 
-        for(int x : table[index])
-            if(x==key)
+        for (int x : table[index]) {
+            if (x == key)
                 return true;
+        }
 
         return false;
     }
 
-    void remove(int key)
-    {
+    void remove(int key) {
         int index = hashFunction(key);
 
         auto &bucket = table[index];
 
-        for(auto it=bucket.begin();it!=bucket.end();it++)
-        {
-            if(*it==key)
-            {
+        for (auto it = bucket.begin(); it != bucket.end(); it++) {
+            if (*it == key) {
                 bucket.erase(it);
                 return;
             }
         }
     }
 
-    void display()
-    {
-        for(int i=0;i<SIZE;i++)
-        {
-            cout<<i<<" -> ";
+    void display() {
+        for (int i = 0; i < SIZE; i++) {
+            cout << i << " -> ";
 
-            for(int x : table[i])
-                cout<<x<<" ";
+            for (int x : table[i])
+                cout << x << " ";
 
-            cout<<endl;
+            cout << endl;
         }
     }
 };
 
-int main()
-{
+int main() {
     HashTable ht;
 
     ht.insert(15);
@@ -67,4 +62,14 @@ int main()
     ht.insert(12);
 
     ht.display();
+
+    cout << "\nSearch 25: " << (ht.search(25) ? "Found" : "Not Found") << endl;
+    cout << "Search 100: " << (ht.search(100) ? "Found" : "Not Found") << endl;
+
+    ht.remove(25);
+
+    cout << "\nAfter removing 25:\n";
+    ht.display();
+
+    return 0;
 }
