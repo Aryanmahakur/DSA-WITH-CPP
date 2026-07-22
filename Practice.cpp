bb03657174a53d209ac1a6d64a26bbd7cfe14a3f
Aryanmahakur <mahakuraryan@gmail.com>
2026-07-22 17:29:01 +0530

    uf

--- Practice.cpp
+++ Practice.cpp
@@ -1,58 +1,34 @@
-#include <iostream>
-#include <stack>
+#include<iostream>
 using namespace std;
-
-struct ListNode {
-    int val;
-    ListNode* next;
-
-    ListNode(int x) {
-        val = x;
-        next = nullptr;
+#include<string>
+class treenode{
+    public:
+    treenode* children[26];
+    bool isend;
+    treenode(){
+         isend=false;
+         for(int i=0;i<26;i++){
+            children[i]=NULL;
+         }
+    }
+ 
+};
+class trie{
+    public:
+       treenode* root;
+    trie(){
+        root=new treenode();
+    }
+    void insert(string word){
+        treenode* node=root;
+        for(char c : word){
+            int index=c-'a';
+           if(node->children[index]==NULL){
+            node->children[index]=new treenode();
+            node=node->children[index];
+           }
+        }
+        node->isend=true;
     }
 };
-
-class Solution {
-public:
-    void reorderList(ListNode* head) {
-        ListNode* curr = head;
-        stack<int> s1;
-        int index = 0;
-
-        while (curr != nullptr) {
-            s1.push(curr->val);
-            index++;
-            curr = curr->next;
-        }
-
-        curr = head;
-        int flag = 0;
-
-        for (int i = 0; i < index; i++) {
-            if (flag == 0) {
-                cout << curr->val << " ";
-                curr = curr->next;
-                flag = 1;
-            }
-            else if (flag == 1) {
-                cout << s1.top() << " ";
-                s1.pop();
-                flag = 0;
-            }
-        }
-    }
-};
-
-int main() {
-    // Create: 1 -> 2 -> 3 -> 4 -> 5
-    ListNode* head = new ListNode(1);
-    head->next = new ListNode(2);
-    head->next->next = new ListNode(3);
-    head->next->next->next = new ListNode(4);
-    head->next->next->next->next = new ListNode(5);
-
-    Solution obj;
-    obj.reorderList(head);
-
-    return 0;
-}
+bool search(string wor)
--- Trie.cpp
+++ Trie.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <string>
+using namespace std;
 class TrieNode {
 public:
     TrieNode* children[26];
@@ -63,3 +66,30 @@ public:
         return true;
     }
 };
+int main() {
+    Trie trie;
+
+    // Insert words
+    trie.insert("cat");
+    trie.insert("car");
+    trie.insert("dog");
+
+    // Search complete words
+    cout << "search(cat): " << trie.search("cat") << endl;   // 1
+    cout << "search(car): " << trie.search("car") << endl;   // 1
+    cout << "search(ca): " << trie.search("ca") << endl;     // 0
+    cout << "search(cap): " << trie.search("cap") << endl;   // 0
+    cout << "search(dog): " << trie.search("dog") << endl;   // 1
+    cout << "search(do): " << trie.search("do") << endl;     // 0
+
+    cout << endl;
+
+    // Check prefixes
+    cout << "startsWith(ca): " << trie.startsWith("ca") << endl;   // 1
+    cout << "startsWith(car): " << trie.startsWith("car") << endl; // 1
+    cout << "startsWith(do): " << trie.startsWith("do") << endl;   // 1
+    cout << "startsWith(da): " << trie.startsWith("da") << endl;   // 0
+    cout << "startsWith(z): " << trie.startsWith("z") << endl;     // 0
+
+    return 0;
+}
