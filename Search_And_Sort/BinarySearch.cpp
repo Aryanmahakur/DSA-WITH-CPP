956b9b25c65919b3e9328af8daa3c98ee90083b8
Aryanmahakur <mahakuraryan@gmail.com>
2026-09-01 14:23:44 +0530

    uf

---
+++ Search_And_Sort/BinarySearch.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int binarySearch(vector<int>& arr, int target) {
+    int low = 0;
+    int high = arr.size() - 1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == target)
+            return mid;
+
+        else if (arr[mid] < target)
+            low = mid + 1;
+
+        else
+            high = mid - 1;
+    }
+
+    return -1;
+}
+
+int main() {
+    vector<int> arr = {10, 20, 30, 40, 50};
+
+    int target = 40;
+
+    cout << binarySearch(arr, target);
+
+    return 0;
+}
